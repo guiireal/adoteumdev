672934c242f0e6c1b30aa317eef8b3553cccff4a
Guilherme França <guii_real@hotmail.com>
2021-07-25 03:36:26 -0300

    chore: implement login github

--- app/Http/Livewire/Components/HomeScreen.php
+++ app/Http/Livewire/Components/HomeScreen.php
@@ -6,6 +6,15 @@ use Livewire\Component;
 
 class HomeScreen extends Component
 {
+    public function loginAsRecruiter()
+    {
+    }
+
+    public function loginAsDev()
+    {
+        return redirect()->route('auth.github.redirect');
+    }
+
     public function render()
     {
         return view('livewire.components.home-screen');
